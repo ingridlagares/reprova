fb772a2f83a602da73722b4194d6ac5accf97f81
Ingrid Lagares <ingridlagares@hotmail.com>
2022-11-24 09:03:49 -0300

    Rename DAO field in GetQUestionHandler

--- src/java/br/ufmg/engsoft/reprova/services/handlers/GetQuestionsHandler.java
+++ src/java/br/ufmg/engsoft/reprova/services/handlers/GetQuestionsHandler.java
@@ -10,11 +10,11 @@ import br.ufmg.engsoft.reprova.services.output.GetQuestionsOutput;
 
 public class GetQuestionsHandler implements IGetQuestionsHandler {
 
-	private static QuestionDAO DAO =  QuestionDAO.getInstance();
+	private static QuestionDAO dataAccess =  QuestionDAO.getInstance();
 
 	@Override
 	public GetQuestionsOutput handle(GetQuestionsInput input) {
-		Collection<Question> questions = DAO.list(null, input.getAuth() ? null : false);
+		Collection<Question> questions = dataAccess.list(null, input.getAuth() ? null : false);
 
     return new GetQuestionsOutput(questions);
 	}
